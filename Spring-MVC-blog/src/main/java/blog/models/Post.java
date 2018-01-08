b60fd23251b86158478aad536e7b9324ca3e2549
Shuowen Wei <Shuowen.Wei@finra.org>
2018-01-08 18:11:00 -0500

    this is a workable version, add Post and User to local Mysql DB (blog_db)

--- Spring-MVC-blog/src/main/java/blog/models/Post.java
+++ Spring-MVC-blog/src/main/java/blog/models/Post.java
@@ -1,12 +1,25 @@
 package blog.models;
+
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "posts")
 public class Post {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 300)
     private String title;
+
+    @Lob @Column(nullable = false)
     private String body;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private User author;
+
+    @Column(nullable = false)
     private Date date = new Date();
 
     public Long getId() {
--- Spring-MVC-blog/src/main/java/blog/models/User.java
+++ Spring-MVC-blog/src/main/java/blog/models/User.java
@@ -1,14 +1,28 @@
 package blog.models;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 30, unique = true)
     private String username;
+
+    @Column(length = 60)
     private String passwordHash;
+
+    @Column(length = 100)
     private String fullName;
-    private Set<Post> posts = new HashSet<>();
+
+    @OneToMany(mappedBy = "author")
+    private Set<Post> posts = new HashSet<Post>();
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
