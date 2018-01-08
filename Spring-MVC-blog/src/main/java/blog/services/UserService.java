1b5e19579894c52e3ab9749eb1db06412ec400ee
Shuowen Wei <Shuowen.Wei@finra.org>
2018-01-08 18:48:49 -0500

    this a workable version with connection to local Mysql DB (blog_db)

--- Spring-MVC-blog/src/main/java/blog/services/UserService.java
+++ Spring-MVC-blog/src/main/java/blog/services/UserService.java
@@ -1,5 +1,16 @@
 package blog.services;
 
+import blog.models.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    List<User> findAll();
+    User findById(Long id);
+    User create(User user);
+    User edit(User user);
+    void deleteById(Long id);
+
     boolean authenticate(String username, String password);
 }
--- Spring-MVC-blog/src/main/java/blog/services/UserServiceJpaImpl.java
+++ Spring-MVC-blog/src/main/java/blog/services/UserServiceJpaImpl.java
@@ -3,13 +3,16 @@ package blog.services;
 import blog.models.User;
 import blog.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
 
 @Service
-public class UserServiceStubImpl implements UserService {
+@Primary
+public class UserServiceJpaImpl implements UserService {
+
     @Autowired
     private UserRepository userRepo;
 
